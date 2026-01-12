52b0011b262090c598e5b9ec13e11c4e284da877
João Pedro <joao.guadalupe06@gmail.com>
2026-01-11 21:33:59 -0300

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 import view.*;
-
 import javax.swing.*;
 import sistema.Hospital;
+
+/*
+    João Pedro Lemos Guadalupe
+    Matrícula: 202435004
+
+    Davi Moljo Domingues
+    Matrícula: 202435031
+
+    Carlos Roberto da Silva
+    Matrícula: 202435016
+*/
 
 public class Main {
     public static void main(String[] args) {
